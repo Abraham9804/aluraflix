08040b4bf73ba97a7e84ebde1112a3b142d73132
Abraham Romero <romero9804@hotmail.com>
2025-01-10 21:32:19 -0800

    creacion de estado categorias

--- src/components/Videos/index.jsx
+++ src/components/Videos/index.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react"
 import Secction from "../Section"
 
 const Videos = () => {
+    const [categorias, setCategorias] = useState([])
+    useEffect(()=>{
+        fetch("http://localhost:3000/videos")
+        .then(response => response.json())
+        .then(data => {
+            
+           const catRepetidas = data.map(video => video.categoria )
+           const catUnicas = [...new Set(catRepetidas)]
+           return setCategorias(catUnicas)
+        })
+        .catch(error => console.log(error))
+    },[])
+
     
     return (
         <>
-        <Secction></Secction>
+        
+        {
+            categorias.map(categoria => <Secction key={categoria}></Secction>)
+        }
+        
         </>
     )
 }
